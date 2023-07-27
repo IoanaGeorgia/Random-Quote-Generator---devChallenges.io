150df8f4be37840b790440d3b564a409f9caf5e9
IoanaGeorgia <91886439+IoanaGeorgia@users.noreply.github.com>
2023-07-27 21:16:03 +0300

    styling for the random quote done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,133 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import './style.css';
 
 export default function App() {
+
+  const [isHover, setIsHover] = useState(false);
+  const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [genre, setGenre] = useState(null)
+
+  useEffect(() => {
+      getQuote()
+  }, []);
+
+  const hoverInputEnter = () => {
+    setIsHover(true);
+  };
+
+  const hoverInputLeave = () => {
+    setIsHover(false);
+  };
+
+  function getQuote(){
+    fetch(' https://quote-garden.onrender.com/api/v3/quotes/random')
+    .then(response => response.json())
+    .then(json => {console.log(json), setQuote(json.data[0].quoteText), setAuthor(json.data[0].quoteAuthor), setGenre(json.data[0].quoteGenre)})
+    .catch(error => console.error(error));
+  }
+  function styleArrow(){
+    if(isHover){
+      return {...quoteInfo, ...hoverInfo}
+    } else{
+    return quoteInfo
+    }
+  }
+  function authorStyle(){
+    if(isHover){
+      return {...authorStyleDefault, ...authorHover}
+
+    }else{
+      return authorStyleDefault
+    }
+  }
+  function getMoreQuotes(){
+    return
+  }
+  const mainWrapper = {
+    display:'grid',
+    'place-items':'center',
+    padding:'2vh 10vh',
+    'font-family':'Arial'
+  }
+  const headerStyles ={
+    'text-align':'right',
+    width:'100%',
+    'padding-bottom':'20vh'
+  }
+  const refreshButton = {
+      background:'transparent',
+      border:'none',
+      color:'gray',
+      cursor:'pointer'
+  }
+  const refreshIcon = {
+    transform: "rotate(90deg)",
+    display:'inline-block'
+  }
+  const quoteMainWrapper ={
+    height:'50vh',
+    width:'60vh',
+  }
+  const quoteWrapper={
+    'border-left':'5px solid #f5cb42',
+    'padding-left':'8vh',
+    'margin-bottom':'15vh',
+    'font-size':'23px'
+  }
+  const quoteInfo={
+    'margin-left':'7vh',
+    'padding-bottom':'5vh',
+    'margin-bottom':'10vh',
+    'padding-top':'4vh',
+    'padding-right':'3vh',
+    'padding-left':'2vh',
+    display:'flex',
+    'justify-content':'space-between',
+    'align-items':'center'
+  }
+  const authorStyleDefault= {
+    'font-weight':'bolder',
+    'font-size':'14px',
+    }
+  const genreStyle = {
+    color:'grey',
+    'font-size':'12px',
+    'padding-top':'1vh',
+  
+  }
+  const quoteInfoArrow={
+    color:'white'
+  }
+  const hoverInfo ={
+    background:'black',
+    cursor:'pointer'
+  }
+  const authorHover ={
+    color:'white'
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={mainWrapper}>
+      <div style={headerStyles}>
+        <button style={refreshButton}> random <div style={refreshIcon}>🗘</div> </button>
+      </div>
+      <div style={quoteMainWrapper}>
+        <div style={quoteWrapper}>
+            {JSON.stringify(quote)}
+          </div>
+          <div style={styleArrow()}                 
+            onMouseEnter={hoverInputEnter}
+            onMouseLeave={hoverInputLeave}>
+            <span>
+            <div style={authorStyle()}>{author}</div>
+            <div style={genreStyle}>{genre}</div>
+            </span>
+            <div style={quoteInfoArrow}
+             >→</div>
+            
+          </div>
+      </div>
     </div>
   );
 }
